b8b9aec1f6c0d241bb8d54fce70d4a84f37ef4c0
Alastair Brunton <info@simplyexcited.co.uk>
2009-02-10 10:42:09 +0100

    Completing the tests.

--- test/basecamp_test.rb
+++ test/basecamp_test.rb
@@ -5,29 +5,16 @@ context "The Basecamp notifier" do
   setup do
     @config = {}
     @notifier = Integrity::Notifier::Basecamp
+    @basecamp = BasecampApi
   end
 
   test "alerts Basecamp on build" do
-    @notifier.any_instance.expects(:room).at_least_once.returns mock(:speak => true)
-    @notifier.any_instance.expects(:short_message).returns('')
-    @notifier.any_instance.expects(:build_url).returns('')
+    @notifier.any_instance.stubs(:short_message).returns('')
+    @notifier.any_instance.stubs(:build_url).returns('')
+    @notifier.any_instance.expects(:full_message).returns('')
+    @basecamp.any_instance.expects(:post_message).returns(true)
     @notifier.notify_of_build(stub_everything(:failed? => false, :project => stub_everything), @config)
   end
-
-
-  #
-  #   *test* "sends the full message on failure only" do
-  #     @notifier.any_instance.expects(:room).at_least_once.returns mock \
-  #       :speak => true,
-  #       :paste => true
-  #
-  #     @notifier.any_instance.expects(:full_message).returns('')
-  #     @notifier.any_instance.expects(:short_message).returns('')
-  #     @notifier.any_instance.expects(:build_url).returns('')
-  #
-  #     @notifier.notify_of_build(stub_everything(:failed? => true, :project => stub_everything), @config)
-  #
-  #   end
 
   test "renders a haml config file" do
     haml = Integrity::Notifier::Basecamp.to_haml
